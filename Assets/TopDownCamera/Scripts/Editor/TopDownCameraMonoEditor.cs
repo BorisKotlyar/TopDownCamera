eb9b92722d16ff7bf638f972805c7fbcbc3d02a5
Boris <boris.kotlyar@zimad.com>
2019-09-01 01:30:18 +0300

    Basic top down camera

---
+++ Assets/TopDownCamera/Scripts/Editor/TopDownCameraMonoEditor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using UnityEngine;
+
+namespace TopDownCamera.Scripts.Editor
+{
+    using UnityEditor;
+    
+    [CustomEditor(typeof(TopDownCameraMono))]
+    public class TopDownCameraMonoEditor : Editor
+    {
+        private TopDownCameraMono _target;
+        private GUIStyle _labelStyle;
+        
+        private void OnEnable()
+        {
+            _target = target as TopDownCameraMono;
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            
+        }
+
+        private void OnSceneGUI()
+        {
+            // Properties and checkers
+            var cameraTargetProp = serializedObject.FindProperty("_target");
+            if (cameraTargetProp == null || cameraTargetProp.objectReferenceValue == null)
+            {
+                return;
+            }
+            
+            var cameraTarget = cameraTargetProp.objectReferenceValue as Transform;
+            if (cameraTarget == null)
+            {
+                return;
+            }
+            
+            var targetPosition = cameraTarget.position;
+            var distance = serializedObject.FindProperty("_distance");
+            var height = serializedObject.FindProperty("_height");
+            var forward = cameraTarget.forward;
+
+            // Distance disc
+            Handles.color = new Color(1f, 0f, 0f, 0.15f);
+            Handles.DrawSolidDisc(targetPosition, Vector3.up, distance.floatValue);
+            
+            Handles.color = new Color(0f, 1f, 0f, 0.75f);
+            Handles.DrawWireDisc(targetPosition, Vector3.up, distance.floatValue);
+
+            // Create Slider handles to adjust camera properties
+            Handles.color = new Color(0f, 1f, 0f, 0.5f);
+            distance.floatValue = Handles.ScaleSlider(
+                distance.floatValue, 
+                targetPosition, 
+                -forward,
+                Quaternion.identity, 
+                distance.floatValue, 
+                1f);
+            distance.floatValue = Mathf.Clamp(distance.floatValue, 5f, float.MaxValue);
+                
+            Handles.color = new Color(0f, 0f, 1f, 0.5f);
+            height.floatValue = Handles.ScaleSlider(
+                height.floatValue, 
+                targetPosition, 
+                Vector3.up, 
+                Quaternion.identity, 
+                height.floatValue, 
+                1f);
+            height.floatValue = Mathf.Clamp(height.floatValue, 5f, float.MaxValue);
+
+            InitStyles();
+
+            _labelStyle.alignment = TextAnchor.UpperCenter;
+            Handles.Label(targetPosition + (-forward * distance.floatValue), "Distance");
+            
+            _labelStyle.alignment = TextAnchor.MiddleRight;
+            Handles.Label(targetPosition + (Vector3.up * height.floatValue), "Height");
+
+            _target.HandleCamera();
+            
+            // save and update values
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void InitStyles()
+        {
+            if (_labelStyle != null)
+            {
+                return;
+            }
+
+            _labelStyle = new GUIStyle
+            {
+                fontSize = 15, 
+                normal = {textColor = Color.white}
+            };
+        }
+    }
+}
---
+++ Assets/TopDownCamera/Scripts/TopDownCameraMono.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace TopDownCamera.Scripts
+{
+    public class TopDownCameraMono : MonoBehaviour
+    {
+        [SerializeField] private Transform _cameraTransform;
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _height;
+        [SerializeField] private float _distance;
+        [SerializeField] private float _angle;
+
+        [SerializeField] private float _smoothSpeed;
+        
+        private Vector3 _refVelocity;
+        
+        protected void Start()
+        {
+            HandleCamera();
+        }
+
+        protected void Update()
+        {
+            HandleCamera();
+        }
+
+        public virtual void HandleCamera()
+        {
+            if (_target == null)
+            {
+                return;
+            }
+
+            var worldPosition = (Vector3.forward * _distance) + (Vector3.up * _height);
+            var rotateVector = Quaternion.AngleAxis(_angle, Vector3.up) * worldPosition;
+            
+            var flatTargetPosition = _target.position;
+            flatTargetPosition.y = 0f;
+
+            var finalPosition = flatTargetPosition + rotateVector;
+
+            _cameraTransform.position = Vector3.SmoothDamp(_cameraTransform.position, finalPosition, ref _refVelocity, _smoothSpeed);
+            _cameraTransform.LookAt(flatTargetPosition);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(0f, 1f, 0f, 0.25f);
+            if (_target != null)
+            {
+                Gizmos.DrawLine(_cameraTransform.position, _target.position);
+                Gizmos.DrawSphere(_target.position, 1.5f);
+            }
+            
+            Gizmos.DrawSphere(_cameraTransform.position, 1.5f);
+        }
+    }
+}
